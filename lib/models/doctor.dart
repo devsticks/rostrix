40b3317244eba3d769a09b5bece5acef892319dd
Devin Stickells <devsticks@gmail.com>
2024-06-30 17:42:46 +0200

    test: add thorough doctor model unit tests

--- lib/models/doctor.dart
+++ lib/models/doctor.dart
@@ -20,8 +20,8 @@ class Doctor {
     this.caesarCoverWeekdayCalls = 0,
     this.weekendCalls = 0,
     this.caesarCoverWeekendCalls = 0,
-    this.leaveDays = const [],
-  });
+    List<DateTime>? leaveDays,
+  }) : leaveDays = leaveDays ?? [];
 
   Doctor copy() {
     return Doctor(
@@ -36,5 +36,42 @@ class Doctor {
       caesarCoverWeekendCalls: caesarCoverWeekendCalls,
       leaveDays: List.from(leaveDays),
     );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Doctor &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          canPerformCaesars == other.canPerformCaesars &&
+          canPerformAnaesthetics == other.canPerformAnaesthetics &&
+          overtimeHours == other.overtimeHours &&
+          overnightWeekdayCalls == other.overnightWeekdayCalls &&
+          secondOnCallWeekdayCalls == other.secondOnCallWeekdayCalls &&
+          caesarCoverWeekdayCalls == other.caesarCoverWeekdayCalls &&
+          weekendCalls == other.weekendCalls &&
+          caesarCoverWeekendCalls == other.caesarCoverWeekendCalls &&
+          _listEquals(leaveDays, other.leaveDays);
+
+  @override
+  int get hashCode =>
+      name.hashCode ^
+      canPerformCaesars.hashCode ^
+      canPerformAnaesthetics.hashCode ^
+      overtimeHours.hashCode ^
+      overnightWeekdayCalls.hashCode ^
+      secondOnCallWeekdayCalls.hashCode ^
+      caesarCoverWeekdayCalls.hashCode ^
+      weekendCalls.hashCode ^
+      caesarCoverWeekendCalls.hashCode ^
+      leaveDays.hashCode;
+
+  bool _listEquals(List<DateTime> list1, List<DateTime> list2) {
+    if (list1.length != list2.length) return false;
+    for (int i = 0; i < list1.length; i++) {
+      if (list1[i] != list2[i]) return false;
+    }
+    return true;
   }
 }
--- test/models/doctor_test.dart
+++ test/models/doctor_test.dart
@@ -13,6 +13,229 @@ void main() {
       expect(doctor.canPerformAnaesthetics, false);
     });
 
-    // Add more tests as needed
+    test('Hours can be added to a doctor', () {
+      final doctor = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor.overtimeHours = 10;
+      expect(doctor.overtimeHours, 10);
+    });
+
+    test('Shifts can be added to a doctor', () {
+      final doctor = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor.overnightWeekdayCalls = 1;
+      doctor.secondOnCallWeekdayCalls = 2;
+      doctor.caesarCoverWeekdayCalls = 3;
+      doctor.weekendCalls = 4;
+      doctor.caesarCoverWeekendCalls = 5;
+      expect(doctor.overnightWeekdayCalls, 1);
+      expect(doctor.secondOnCallWeekdayCalls, 2);
+      expect(doctor.caesarCoverWeekdayCalls, 3);
+      expect(doctor.weekendCalls, 4);
+      expect(doctor.caesarCoverWeekendCalls, 5);
+    });
+
+    test('Leave days can be added to a doctor', () {
+      Doctor doctor = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor.leaveDays.add(DateTime.now());
+      expect(doctor.leaveDays.length, 1);
+    });
+
+    test('Doctor can be copied', () {
+      final doctor = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final copy = doctor.copy();
+      expect(copy.name, doctor.name);
+      expect(copy.canPerformCaesars, doctor.canPerformCaesars);
+      expect(copy.canPerformAnaesthetics, doctor.canPerformAnaesthetics);
+      expect(copy.overtimeHours, doctor.overtimeHours);
+      expect(copy.overnightWeekdayCalls, doctor.overnightWeekdayCalls);
+      expect(copy.secondOnCallWeekdayCalls, doctor.secondOnCallWeekdayCalls);
+      expect(copy.caesarCoverWeekdayCalls, doctor.caesarCoverWeekdayCalls);
+      expect(copy.weekendCalls, doctor.weekendCalls);
+      expect(copy.caesarCoverWeekendCalls, doctor.caesarCoverWeekendCalls);
+      expect(copy.leaveDays, doctor.leaveDays);
+    });
+
+    test('Doctor can be compared', () {
+      final today = DateTime.now();
+      Doctor doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor1.overtimeHours = 10;
+      doctor1.overnightWeekdayCalls = 1;
+      doctor1.secondOnCallWeekdayCalls = 2;
+      doctor1.caesarCoverWeekdayCalls = 3;
+      doctor1.weekendCalls = 4;
+      doctor1.caesarCoverWeekendCalls = 5;
+      doctor1.leaveDays.add(today);
+      Doctor doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.overtimeHours = 10;
+      doctor2.overnightWeekdayCalls = 1;
+      doctor2.secondOnCallWeekdayCalls = 2;
+      doctor2.caesarCoverWeekdayCalls = 3;
+      doctor2.weekendCalls = 4;
+      doctor2.caesarCoverWeekendCalls = 5;
+      doctor2.leaveDays.add(today);
+      expect(doctor1 == doctor2, true);
+    });
+
+    test('Doctor can be compared for identical objects', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      Doctor doctor2 = doctor1;
+      expect(identical(doctor1, doctor2), true);
+    });
+
+    test('Doctor can be compared with different skill values', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: true);
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different overtime hours values', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.overtimeHours = 10;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different weekday overnight values', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.overnightWeekdayCalls = 1;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different weekday 2nd-on-call values',
+        () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.secondOnCallWeekdayCalls = 2;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test(
+        'Doctor can be compared with different weekday caesar cover call values',
+        () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.caesarCoverWeekdayCalls = 3;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different weekend call values', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.weekendCalls = 4;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test(
+        'Doctor can be compared with different weekend caesar cover call values',
+        () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.caesarCoverWeekendCalls = 5;
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different leave days values', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      doctor2.leaveDays.add(DateTime.now());
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with different names', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      final doctor2 = Doctor(
+          name: 'Dr. Test 2',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      expect(doctor1 == doctor2, false);
+    });
+
+    test('Doctor can be compared with null', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      expect(doctor1 == null, false);
+    });
+
+    test('Doctor can be compared with itself', () {
+      final doctor1 = Doctor(
+          name: 'Dr. Test',
+          canPerformCaesars: true,
+          canPerformAnaesthetics: false);
+      expect(doctor1 == doctor1, true);
+    });
   });
 }
